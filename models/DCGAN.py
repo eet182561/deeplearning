863e83783e84256793616460fc3991f60a185e5e
Abhishek <abhishek.bohra@outlook.com>
2020-04-24 00:13:26 +0530

    Minor bug fixes GAN

--- models/DCGAN.py
+++ models/DCGAN.py
@@ -4,6 +4,7 @@ from keras.optimizers import RMSprop,Adam
 from keras.losses import BinaryCrossentropy
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 class GAN:
     def __init__(
@@ -66,25 +67,25 @@ class GAN:
 
     def _build_discriminator(self):
         # Discriminator
-        discriminator_input = Input(input_shape=self.discriminator_input,name = 'discriminator_input')
+        discriminator_input = Input(shape=self.input_dim,name = 'discriminator_input')
+        print('input dim',self.input_dim)
         x = discriminator_input
         
-        for i in len(self.discriminator_conv_filter):
+        for i in range(len(self.discriminator_conv_filter)):
             conv = Conv2D(filters = self.discriminator_conv_filter[i],
                         kernel_size=self.discriminator_conv_kernel_size[i],
                         padding='same',
-                        activation=self.disctriminator_activation_function,
-                        strides=(self.discriminator_conv_stride[i],self.discriminator_conv_stride[i]),
+                        strides=self.discriminator_conv_stride[i],
                         name = 'Discriminator_conv_layer_'+str(i+1))
             x = conv(x)
             if self.discriminator_batch_norm_momentum : 
-                bn = BatchNormalisation(momentum = self.discriminator_batch_norm_momentum,name = 'Batch_norm_layer_'+str(i+1))
+                bn = BatchNormalization(momentum = self.discriminator_batch_norm_momentum,name = 'Batch_norm_layer_'+str(i+1))
                 x = bn(x)
             x = Activation(self.disctriminator_activation_function)(x)
             if self.discriminator_dropout_rate:
                 x = Dropout(self.discriminator_dropout_rate)(x)
         x = Flatten()(x)        
-        discriminator_output = Dense(1,activation='sigmoid')
+        discriminator_output = Dense(1,activation='sigmoid')(x)
         self.discriminator = Model(discriminator_input,discriminator_output)
 
     def get_opti(self, lr):
@@ -101,13 +102,13 @@ class GAN:
         x = generator_input
         x = Dense(np.prod(self.generator_initial_dense_layer_size))(x)
         if self.generator_batch_norm_momentum:
-            x = BatchNormalisation(momentum = self.generator_batch_norm_momentum)(x)
+            x = BatchNormalization(momentum = self.generator_batch_norm_momentum)(x)
         x = Activation(self.genrator_activation)(x)
         x = Reshape(self.generator_initial_dense_layer_size)(x)
         if self.genrator_dropout_rate:
             x = Dropout(rate = self.genrator_dropout_rate)(x)
         for i in range(len(self.generator_conv_filter)):
-            if self.generator_upsample[i]==2
+            if self.generator_upsample[i]==2:
                 x = UpSampling2D()(x)
                 x = Conv2D(
                     filters=self.generator_conv_filter[i],
@@ -117,17 +118,16 @@ class GAN:
                     )(x)
             else:
                 x = Conv2DTranspose(
-                    filters = self.generator_conv_filters[i]
+                    filters = self.generator_conv_filter[i]
                     , kernel_size = self.generator_conv_kernel_size[i]
                     , padding = 'same'
                     , strides = self.generator_conv_strides[i]
                     , name = 'generator_conv_' + str(i)
                     )(x)
-                )
 
             if i<len(self.generator_conv_filter)-1:
                 if self.generator_batch_norm_momentum:
-                    x = BatchNormalisation(momentum = self.generator_batch_norm_momentum)(x)
+                    x = BatchNormalization(momentum = self.generator_batch_norm_momentum)(x)
                 x = Activation(self.genrator_activation)(x)
             else:
                 x = Activation('tanh')(x)
@@ -140,18 +140,19 @@ class GAN:
         for layer in model.layers:
             layer.trainable = value
 
-    def _build_adverserial():
+    def _build_adverserial(self):
         # Training the discriminator
 
         self.discriminator.compile(
-            optimiser = RMSprop(lr = 0.0008),
+            optimizer = RMSprop(lr = 0.0008),
             loss = 'binary_crossentropy',
             metrics = ['accuracy']
         )
 
         self.set_trainable(self.discriminator,False)
+
         model_input = Input(shape=(self.zdim,),name = 'model_input')
-        model_output = self.discriminator(self.generator)
+        model_output = self.discriminator(self.generator(model_input))
         self.model = Model(model_input,model_output)
         self.model.compile(
             optimizer = self.get_opti(self.generator_learning_rate),
@@ -164,11 +165,11 @@ class GAN:
     def train_discriminator(self,x_train,batch_size,using_generator):
         #Prepare labels
         valid = np.ones(shape=(batch_size,1))
-        fake = np.zeroes(shape=(batch_size,1))
+        fake = np.zeros(shape=(batch_size,1))
         
         #prepare real iamges
         if using_generator:
-            true_images = next(x_train)[0] #next return (x,y); 0 for x
+            true_images = next(x_train) #next return (x,y); 0 for x
             if true_images.shape[0] != batch_size:
                 true_images = next(x_train)[0]
         else:
@@ -177,15 +178,14 @@ class GAN:
 
         #prepare fake ones generated by generator
         noise = np.random.normal(0,1,size = (batch_size,self.zdim))
-        fake_images = self.genrator.predict(noise)
-
+        fake_images = self.generator.predict(noise)
         d_loss_true,d_acc_true = self.discriminator.train_on_batch(true_images,valid)
         d_loss_fake,d_acc_fake = self.discriminator.train_on_batch(fake_images,fake)
 
         d_loss = 0.5 *(d_loss_true + d_loss_fake)
         d_acc = 0.5*(d_acc_fake + d_acc_true)
         
-        return [d_loss,d_loss_real,d_loss_fake,d_acc,d_acc_real,d_acc_fake]
+        return [d_loss,d_loss_true,d_loss_fake,d_acc,d_acc_true,d_acc_fake]
 
     def train_generator(self,batch_size):
         valid = np.ones((batch_size,1))
@@ -194,7 +194,7 @@ class GAN:
 
     def sample_images(self,run_folder):
         r, c = 5, 5
-        noise = np.random.normal(0, 1, (r * c, self.z_dim))
+        noise = np.random.normal(0, 1, (r * c, self.zdim))
         gen_imgs = self.generator.predict(noise)
 
         gen_imgs = 0.5 * (gen_imgs + 1)
@@ -216,15 +216,16 @@ class GAN:
             d = self.train_discriminator(x_train,batch_size,using_generator)
             g = self.train_generator(batch_size)
 
-            print("{self.epoch:%d} ,[D Loss={d[0]:.3f},{d[1]:.3f},{d[2]:.3f}] [D acc = {d[3]:.3f},{d[4]:.3f},{d[5]:.3f}] [G loss = {g[0]:.3f} G acc = {g[1]:.3f}]")
+            print("%d ,[D Loss=%.3f,%.3f,%.3f] [D acc = %.3f,%.3f,%.3f] [G loss = %.3f} G acc = %.3f}]"%(self.epoch,d[0],d[1],d[2],d[3],d[4],d[5],g[0],g[1]))
             self.d_losses.append(d)
-            self.g_losses.append(d)
+            self.g_losses.append(g)
             g_best = None 
             if (epoch)%print_every_n_batch == 0:
                 self.sample_images(run_folder)
                 self.model.save_weights(os.path.join(run_folder, 'weights/weights-%d.h5' % (epoch)))
-                if g[0] < g_best or g_best is None:
+                if g_best is None or g[0]<g_best:
                     self.model.save_weights(os.path.join(run_folder, 'weights/weights_best_at_epoch_{self.epoch:%5d}.h5'))
                     if g_best is not None:
                         os.remove(os.path.join(run_folder, 'weights/weights_best_at_epoch_{g_best:%5d}.h5'))
                     g_best = self.epoch 
+            self.epoch=epoch+1
